616e8529140cdc5b2eee4c4ba771f08f45f559e8
ptraon <ptraon@gmail.com>
2018-10-23 23:00:24 +0200

    premier exercice OC Symfony

--- src/OC/PlatformBundle/Antispam/OCAntispam.php
+++ src/OC/PlatformBundle/Antispam/OCAntispam.php
@@ -12,6 +12,16 @@ namespace OC\PlatformBundle\Antispam;
 
 class OCAntispam
 {
+    private $mailer;
+    private $locale;
+    private $minLength;
+
+    public function __construct(\Swift_Mailer $mailer, $locale, $minLength)
+    {
+        $this->mailer = $mailer;
+        $this->locale = $locale;
+        $this->minLength = (int) $minLength;
+    }
     /**
      * Vérifie si le texte est un spam ou non
      *
@@ -20,6 +30,6 @@ class OCAntispam
      */
     public function isSpam($text)
     {
-        return strlen($text) < 50;
+        return strlen($text) < $this->minLength;
     }
 }
